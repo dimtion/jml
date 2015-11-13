10818f4c61e22885a0fc8a00edd294083f549ebe
Dimtion <zizou.xena@gmail.com>
2015-11-13 18:28:19 +0100

    Pour cyril

--- inputFiles/ourIA/closest.py
+++ inputFiles/ourIA/closest.py
@@ -5,7 +5,9 @@ import lib.interface as interface
 import lib.utils as u
 import lib.algorithms as algo
 
-
+import signal
+from subprocess import check_output
+import os
 IAName = "closest"
 
 (mazeWidth, mazeHeight, mazeMap, preparationTime, turnTime, playerLocation, opponentLocation, coins, gameIsOver) = interface.initGame(IAName)
@@ -22,10 +24,22 @@ def next_way(playerLocation, coins):
     coins.remove(coin)
     return u.way_width(candidates[0], playerLocation, coin)
 
+def get_pid_en(name):
+    return [int(f) for f in check_output(["pidof", name]).split() if int(f) != os.getpid()]
 
 
-def initialisationTurn (mazeWidth, mazeHeight, mazeMap, timeAllowed, playerLocation, opponentLocation, coins) :
+
+def initialisationTurn(mazeWidth, mazeHeight, mazeMap, timeAllowed, playerLocation, opponentLocation, coins) :
     global route
+
+    pid = get_pid_en("python3")
+    interface.debug(pid)
+    for p in pid:
+        try:
+            os.kill(int(p), signal.SIGTSTP)
+        except:
+            pass
+
     route = next_way(playerLocation, coins)
 
 
